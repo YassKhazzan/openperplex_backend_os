6196c7bac4cbc59b9747cb3a318052eaa623df15
yassine khazzan <yassine.khazzan@gmail.com>
2024-08-03 20:43:25 +0100

    Update semantic_chunking.py

--- semantic_chunking.py
+++ semantic_chunking.py
@@ -5,7 +5,7 @@ from semantic_chunkers import StatisticalChunker
 
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 
-encoder = CohereEncoder(cohere_api_key="xFkq6E9uTlWZYy7zeOhFXmBJc5puhCbbfX9cAk0a", input_type='search_document',
+encoder = CohereEncoder(cohere_api_key=COHERE_API_KEY, input_type='search_document',
                         name='embed-multilingual-v3.0')
 
 chunker = StatisticalChunker(encoder=encoder, max_split_tokens=200)
